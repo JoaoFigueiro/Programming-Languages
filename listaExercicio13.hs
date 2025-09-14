906de03743a4277a1724b1dee78a75d81286b8d1
JoaoFigueiro <joao.l.comiran@gmail.com>
2025-09-14 14:55:12 -0300

    feat: update exercicio 13

--- listaExercicio13.hs
+++ listaExercicio13.hs
@@ -3,3 +3,9 @@ and' True True = True
 and' True False = False
 and' False False = False 
 and' False True = False
+
+or' :: Bool -> Bool -> Bool
+or' True True = True 
+or' True False = True
+or' False False = False 
+or' False True = True
